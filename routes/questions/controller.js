d398faa1a275569f7585eed583ee5f423aa4106b
cktpro <kimtramcap@gmail.com>
2023-08-08 21:14:53 +0700

    questions

--- routes/questions/controller.js
+++ routes/questions/controller.js
@@ -7,181 +7,132 @@ const {
   Order,
 } = require('../../models');
 module.exports = {
-  question1: async (req, res, next) => {
+  question18: async(req,res,next)=>{
     try {
-      const conditionFind={isDeleted:false,discount:{$lte:10}}
-      const result = await Product.find(conditionFind);
-      res.send(200, {
-        message: "Thành công",
+      let result= await Category.aggregate().lookup({
+        from:'products',
+        localField:'_id',
+        foreignField:'categoryId',
+        as:'products',
+      }).unwind(
+        {
+          path:'$products',
+          preserveNullAndEmptyArrays: true
+        }
+      ).group(
+        {
+          _id:'$_id',
+          name:{$first:'$name'},
+          totalProduct:{$sum:1}
+
+        }
+      )
+      .sort(
+        {
+          totalProduct:-1
+        }
+      )
+      if(result.length>0){
+        return res.send({
+        code: 200,
+        mesage: 'Thành công',
         payload: result,
-      });
-    } catch (err) {
-      res.send(500, {
-        message: "Thất bại",
-        error: err,
-      });
-    }
-  },
-  question1b: async (req, res, next) => {
-    const result = await Supplier.find({ isDeleted: false });
-    try {
-      res.send(200, {
-        message: "Thành công",
-        payload: result,
-      });
-    } catch (err) {
-      res.send(400, {
-        message: "Thất bại",
-        error: err,
-      });
-    }
-  },
-  question2: async (req, res, next) => {
-    try {
-      const { name } = req.query;
-      const conditionSearch = { isDeleted: false };
-      if (name) conditionSearch.name = fuzzySearch(name);
-      let result = await Supplier.find(conditionSearch);
-      res.send(200, {
-        message: "Thành công",
-        payload: result
-      });
-    } catch (err) {
-      res.send(404, {
-        message: "Thất bại",
-        error: err,
-      });
-    }
-  },
-  question3: async (req, res, next) => {
-    const { id } = req.params;
-    const itemDetail = itemList.find(
-      (item) => item.id.toString() === id.toString()
-    );
-    if (itemDetail) {
-      if (itemDetail.isDeleted) {
-        return res.send(400, {
-          message: "Sản phẩm đã bị xóa",
         });
       }
-      return res.send(200, {
-        message: "Thành công",
-        payload: itemDetail,
+      return res.status(400).json({
+      code: 404,
+      mesage: 'Không tìm thấy',
       });
-    }
-    return res.send(404, {
-      message: "Không tìm thấy",
-    });
-  },
-  create: async (req, res, next) => {
-    try {
-      const { name, email, phoneNumber, address, isDeleted } = req.body;
-      const newRecord = new Supplier({
-        name,
-        email,
-        phoneNumber,
-        address,
-        isDeleted,
-      });
-      let result = await newRecord.save();
-      return res.send(200, {
-        message: "Thành công",
-        payload: result,
-      });
-    } catch (err) {
-      return res.send(200, {
-        message: "Thất bại",
-        error: err,
-      });
-    }
-  },
-  update: async (req, res, next) => {
-    const { id } = req.params;
-    const { name, email, phoneNumber, address, isDeleted } = req.body;
-    try {
-      let result = await Supplier.findOneAndUpdate(
-        { _id: id, isDeleted: false },
-        { name, email, phoneNumber, address, isDeleted },
-        { new: true }
-      );
-      if (result) {
-        return res.send(200, {
-          message: "Thành công",
-          payload: result,
-        });
-      }
-      return res.send(404, {
-        message: "Không tìm thấy",
-      });
-    } catch (err) {
-      return res.send(400, {
-        message: "Thất bại",
-        error: err,
-      });
-    }
-  },
-  // updatePatch: async (req, res, next) => {
-  //   const { id } = req.params;
-  //   const { name, price, description, discount } = req.body;
-  //   let updateData = {};
-  //   let isErr = false;
-  //   const newProductList = itemList.map((item) => {
-  //     if (item.id.toString() === id.toString()) {
-  //       if (item.isDeleted) {
-  //         isErr = true;
-  //         return item;
-  //       } else {
-  //         updateData = combineObjects(item, {
-  //           name,
-  //           price,
-  //           description,
-  //           discount,
-  //         });
-  //         return updateData;
-  //       }
-  //     }
-  //     return item;
-  //   });
-  //   if (!isErr) {
-  //     await writeFileSync(patch, newProductList);
-  //     return res.send(200, {
-  //       message: "Thành công",
-  //       payload: updateData,
-  //     });
-  //   }
-  //   return res.send(400, {
-  //     message: "Cập nhật không thành công",
-  //   });
-  // },
-  softDelete: async (req, res, next) => {
-    const { id } = req.params;
-    try {
-      const result = await Supplier.findByIdAndUpdate(
-        id,
-        { isDeleted: true },
-        { new: true }
-      );
-      if (result) {
-        return res.send(200, {
-          message: "Thành công xóa",
-        });
-      }
-      return res.send(400, {
-        message: "Thất bại",
-      });
+      
     } catch (error) {
-      return res.send(400, {
-        message: "Thất bại",
-        error: error,
+      return res.status(500).json({
+      code: 500,
+      mesage: 'Thất bại',
+      error: error,
       });
     }
   },
-  // hardDelete: async(req,res,next)=>{
-  //   const {id} =req.params;
-  //   const newProductList=products.filter((item)=>item.id.toString()!==id.toString())
-  //   await writeFileSync(patch,newProductList)
-  //   return res.send(200, {
-  //       message: "Thành công xóa",
-  //     });
-  // }
+  question19: async(req,res,next)=>{
+    try {
+      let result= await Supplier.aggregate().lookup({
+        from:'products',
+        localField:'_id',
+        foreignField:'supplierId',
+        as:'products',
+      })
+      .unwind(
+        {
+          path:'$products',
+          preserveNullAndEmptyArrays: true
+        }
+      )
+      .group(
+        {
+          _id:'$_id',
+          name:{$first:'$name'},
+          totalProduct:{$sum:'$products.stock'}
+
+        }
+      )
+      .sort(
+        {
+          totalProduct:-1
+        }
+      )
+      if(result.length>0){
+        return res.send({
+        code: 200,
+        mesage: 'Thành công',
+        payload: result,
+        });
+      }
+      return res.status(400).json({
+      code: 404,
+      mesage: 'Không tìm thấy',
+      });
+      
+    } catch (error) {
+      return res.status(500).json({
+      code: 500,
+      mesage: 'Thất bại',
+      error: error,
+      });
+    }
+  },
+  question20:async(req,res,next)=>{
+try {
+  const {fromDate,toDate}=req.query;
+  const startDate= new Date(fromDate);
+  const endDate=new Date(toDate)
+  let conditionFind={$expr:{$and:[
+    {$gte:['$createdDate',startDate]},
+    {$lte:['$createdDate',endDate]}
+  ]}}
+let result = await Order.aggregate().match({...conditionFind,status:'WAITING'}).unwind('orderDetails').lookup(
+  {
+    from:'products',
+    localField:'orderDetails.productId',
+    foreignField:'_id',
+    as:'orderDetails.product'
+  }
+)
+if(result.length>0){
+  return res.send({
+  code: 200,
+  mesage: 'Thành công',
+  payload: result,
+  });
+}
+return res.status(400).json({
+code: 404,
+mesage: 'Không tìm thấy',
+});
+} catch (error) {
+  return res.status(500).json({
+  code: 500,
+  mesage: 'Thất bại',
+  error: error,
+  });
+}
+  }
 };
--- routes/questions/router.js
+++ routes/questions/router.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const { validateSchema } = require('../../helper');
-const {question1,question1b}=require('./controller')
+const {question18,question19,question20}=require('./controller')
 const checkIdSchema = require('../validationId')
 const {validationCreateSchema} =require('./validation')
 
 // GET LIST & CREATE LIST
-router.get('/1a', question1);
+router.get('/18', question18);
+router.get('/19', question19);
+router.get('/20', question20);
 
 module.exports= router
