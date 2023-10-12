bf4953116692a086bce7bc6abc0d1c52035d527f
cktpro <kimtramcap@gmail.com>
2023-10-12 19:50:28 +0700

    fix nodejs for vercel

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ const {
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
